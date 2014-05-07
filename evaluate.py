ce6869ad8f8758c42c27926c2286c2c45c50eee2
Brett Jurgens <brett@brettjurgens.com>
2014-05-07 17:44:30 -0500

    should be the final code for me...

--- evaluate.py
+++ evaluate.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python
 
-import time, numpy, os, csv
+import time, numpy, os, csv, math
+from findMotif import gibbs
 
 def main():
     """
     evaluates the motif-finder on the benchmarks
     """
-    # run the benchmarks
+    count = 0
     for root, dirs, files in os.walk('data'):
         for directory in dirs:
+            path = "data/{}".format(directory)
+
+            iterations = 1000000
+            print "Processing folder #{0}/8 with {1} iterations".format(count, iterations)
+
             start_time = time.time()
             # run the gibbs
-            # like here or something...
+            gibbs(path, iterations)
             run_time = time.time() - start_time
 
-            path = "data/{}".format(directory)
+            print "That took {} sec!".format(run_time)
+
             motif_file = open(os.path.join(path, 'motif.txt'), 'r')
             motif = motif_file.readline().split('\t')[2]
             motif_file.close()
@@ -66,7 +73,8 @@ def main():
             overlap = calculate_overlap(sites, predicted_sites)
 
             write_stats(path, run_time, relative_entropy, overlap)
-
+            
+            count += 1
 
 def motif_to_pwm(motif):
     """
@@ -79,7 +87,15 @@ def motif_to_pwm(motif):
     pwm = numpy.zeros((len(motif), 4))
 
     for i, char in enumerate(motif):
-        pwm[i][nucleotides[char]] = 1
+        if char != '*':
+            pwm[i][nucleotides[char]] = 1
+            temp_dict = nucleotides.copy()
+            del temp_dict[char]
+            for nucleotide, index in temp_dict.iteritems():
+                pwm[i][index] = 0.01
+        else:
+            for j in xrange(4):
+                pwm[i][j] = 0.25
 
     return pwm
 
@@ -90,7 +106,8 @@ def relate_entropy(m1i, m2i):
     """
     the_sum = 0
     for j in xrange(4):
-        the_sum += m1i[j] * math.log(m1i[j]/m2i[j])
+        m2ij = float(m2i[j])
+        the_sum += m1i[j] * math.log(m1i[j]/m2ij)
     return the_sum
 
 def compute_relative_entropy(motif, predicted_motif):
@@ -108,12 +125,12 @@ def calculate_overlap(sites, predicted_sites):
     calculate the number of overlapping sites
     between sites.txt and predictedsites.txt
     """
-    overlap = set(sites).intersection(predictedsites)
+    overlap = set(sites).intersection(predicted_sites)
     return len(overlap)
 
 def write_stats(directory, run_time, relative_entropy, overlap):
     f = open(os.path.join(directory, 'stats.txt'), 'w')
-    write_str = '%f\t%f\t%i'.format(run_time, relative_entropy, overlap)
+    write_str = "{0}\t{1}\t{2}".format(run_time, relative_entropy, overlap)
     f.write(write_str)
     f.close()
 

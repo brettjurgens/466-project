73180ddc7a88995b07e19e521530ac30626804c9
Brett Jurgens <brett@brettjurgens.com>
2014-05-07 16:51:17 -0500

    make all data points and run on all of them

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import time, numpy, os
+import time, numpy, os, csv
 
 def main():
     """
@@ -15,19 +15,55 @@ def main():
             run_time = time.time() - start_time
 
             path = "data/{}".format(directory)
-            f = open(os.path.join(path, 'motif.txt'), 'r')
-            motif = f.readline().split('\t')[2]
+            motif_file = open(os.path.join(path, 'motif.txt'), 'r')
+            motif = motif_file.readline().split('\t')[2]
+            motif_file.close()
+
+            motif_length = len(motif)
 
             motif_pwm = motif_to_pwm(motif)
 
-            # @TODO open the predicted motif...
-            predicted_motif = ''
+            # get the predicted motif
+            predicted_motif = []
+            p_motif_file = open(os.path.join(path, 'predictedmotif.txt'), 'r')
+
+            # skip the >PMOTIF line
+            p_motif_file.readline()
+
+            # read it into memory...
+            reader = csv.reader(p_motif_file, delimiter='\t')
+            predicted_motif = list(reader)
+
+            # close the file...
+            p_motif_file.close()
+
+            # remove the > line
+            predicted_motif.pop()
+
+            # remove blank elements from the 2d array
+            for arr in predicted_motif:
+                arr.pop()
 
             relative_entropy = compute_relative_entropy(motif_pwm, predicted_motif)
 
-            # @TODO open the sites and predicted sites...
-            overlap = 0
-            # overlap = calculate_overlap(sites, predicted_sites)
+            # open sites
+            sites_file = open(os.path.join(path, 'sites.txt'), 'r')
+            reader = csv.reader(sites_file, delimiter='\n')
+            sites = list(reader)
+            sites = numpy.array(sites)
+            sites = sites.flatten().tolist()
+            sites_file.close()
+
+            # open predicted sites
+            p_sites_file = open(os.path.join(path, 'sites.txt'), 'r')
+            reader = csv.reader(p_sites_file, delimiter='\n')
+            predicted_sites = list(reader)
+            predicted_sites = numpy.array(predicted_sites)
+            predicted_sites = predicted_sites.flatten().tolist()
+            p_sites_file.close()
+
+            # calculate the overlap
+            overlap = calculate_overlap(sites, predicted_sites)
 
             write_stats(path, run_time, relative_entropy, overlap)
 
@@ -79,7 +115,7 @@ def write_stats(directory, run_time, relative_entropy, overlap):
     f = open(os.path.join(directory, 'stats.txt'), 'w')
     write_str = '%f\t%f\t%i'.format(run_time, relative_entropy, overlap)
     f.write(write_str)
-    f.close
+    f.close()
 
 if __name__ == '__main__':
     main()

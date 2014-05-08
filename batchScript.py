b69e96fcc752b6132fff1d084423d01da2447f53
EC2 Default User <ec2-user@ip-10-203-132-213.ec2.internal>
2014-05-08 10:08:31 +0000

    final numbers

--- batchScript.py
+++ batchScript.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
 import subprocess, os, sys
 
 def main():
 	iterations = sys.argv[1]
+	print iterations
 	for root, dirs, files in os.walk('data'):
 	    for directory in dirs:
 	        args = ['nohup', './evaluate.py', directory, iterations, '&']
